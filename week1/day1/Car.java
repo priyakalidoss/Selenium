e224fbb9dc8181a8990564c10a95d0fc0b1b4292
priyakalidoss <31366358+priyakalidoss@users.noreply.github.com>
2023-12-23 17:23:18 +0530

    Add files via upload

---
+++ week1/day1/Car.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Car {
+	public static void main(String[] args) {
+		
+	}
+
+public void applyBreak() {
+	System.out.println("Applied Break");
+	
+}
+public void soundHorn() {
+	
+	System.out.println("Sound Horn");
+}
+
+}
---
+++ week1/day1/Chrome.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+
+public class Chrome {
+	public void getName() {
+		System.out.println("Get the browser name");
+		
+	}
+	public void printName() {
+		System.out.println("This is google chrome");
+	}
+
+}
