3097b9ac2c9bac28fb6eea62688ae9d2388ed599
priyakalidoss <31366358+priyakalidoss@users.noreply.github.com>
2024-01-01 20:48:30 +0530

    Add files via upload

---
+++ week1/day1/Report.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student student=new Student();
+System.out.println("Student Name:" +student.studentName);
+System.out.println("Student Name:" +student.rollNo);
+System.out.println("Student Name:" +student.collegeName);
+System.out.println("Student Name:" +student.markScored);
+System.out.println("Student Name:" +student.cgpa);
+	}
+
+}
---
+++ week1/day1/Student.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Student {
+	String studentName="Priya";
+	int rollNo=37;
+	String collegeName="rmkcet";
+	int markScored=85;
+	float cgpa=8.1f;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
